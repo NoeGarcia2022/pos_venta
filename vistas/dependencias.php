576e6873c73af6945f2d661182a5138c5e104312
NoeGarcia2022 <noegarcia4789@gmail.com>
2023-08-25 01:09:10 -0600

    Estructura de carpetas, agregando dependencias

---
+++ vistas/dependencias.php
@@ -0,0 +1,12 @@
+<!-- Agragamos links de las librerias que vamos a ocupar -->
+
+<link rel="stylesheet" type="text/css" href="../librerias/alertify/css/alertify.css">
+<link rel="stylesheet" type="text/css" href="../librerias/alertify/css/themes/default.css">
+<link rel="stylesheet" type="text/css" href="../librerias/bootstrap/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="../librerias/select2/css/select2.css">
+
+
+<script src="../librerias/jquery.min.js"></script>
+<script src="../librerias/alertify/alertify.js"></script>
+<script src="../librerias/bootstrap/js/bootstrap.js"></script>
+<script src="../librerias/select2/js/select2.js"></script>
